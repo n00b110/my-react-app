59d594a3bf2dbbdcd8dda3162c8f32254a3d75c1
Immanuel Olaoye <olaoye939@gmail.com>
2024-06-10 21:01:30 -0500

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Home</li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
