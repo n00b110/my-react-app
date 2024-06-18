defbdf50745c4cb2d74a9f8b3fef3dc4ccd42962
Immanuel Olaoye <olaoye939@gmail.com>
2024-06-18 14:40:30 -0500

    module styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import Button from "./Button";
+// There are three ways to apply css styles in React
+// (not including preprocessors or external frameworks like Tailwind or Bootstrap)
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from "./Button/Button";
 
 function App() {
   return <Button />;
